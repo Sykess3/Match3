af3e19677f2a7b047bc2f641e0f962e20ec17f79
Nikolay Bulyha <thesykess@gmail.com>
2022-01-08 20:36:33 +0200

    Added particles static data

---
+++ Assets/_Project/Code/Core/Configs/ParticlesConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _Project.Code.Core.Models.BoardLogic.Cells;
+using _Project.Code.Core.Models.Interfaces.Configs;
+using UnityEngine;
+
+namespace _Project.Code.Core.Configs
+{
+    [CreateAssetMenu(fileName = "", menuName = "StaticData/Particles")]
+    public class ParticlesConfig : ScriptableObject, IParticlesConfig
+    {
+        [SerializeField] private Pair[] _particles;
+
+        public Dictionary<ContentType, ParticleSystem> Particles =>
+            _particles.ToDictionary(x => x.ContentType, x => x.Particle);
+
+        [Serializable]
+        private class Pair
+        {
+            public ContentType ContentType;
+            public ParticleSystem Particle;
+        }
+    }
+}
---
+++ Assets/_Project/Code/Core/Models/Interfaces/Configs/IParticlesConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using _Project.Code.Core.Models.BoardLogic.Cells;
+using UnityEngine;
+
+namespace _Project.Code.Core.Models.Interfaces.Configs
+{
+    public interface IParticlesConfig
+    {
+        Dictionary<ContentType, ParticleSystem> Particles { get; }
+    }
+}
